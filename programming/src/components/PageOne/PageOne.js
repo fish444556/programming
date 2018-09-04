e1d0780960d5d35c921914781594f25bf77b4269
fish444556 <jialinl@mtu.edu>
2018-09-04 10:18:49 -0700

    form add

--- programming/src/components/PageOne/PageOne.js
+++ programming/src/components/PageOne/PageOne.js
@@ -1,11 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const PageOne = () => {
-  return (
-    <div>
-      Page1
-    </div>
-  );
+class PageOne extends Component {
+  constructor(props) {
+    super(props);
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+
+  handleSubmit(event) {
+    console.warn('submit');
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <div>
+            <label>Please input value 1</label>
+            <input name="val1" />
+          </div>
+          <div>
+            <label>Please input value 2</label>
+            <input name="val2" />
+          </div>
+
+          <input type="submit" value="Add" />
+        </form>
+      </div>
+    );
+  }
 };
 
 export default PageOne;
