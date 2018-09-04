c4a6448d3ec98ac85a3213ebc5b83878973e54c5
fish444556 <jialinl@mtu.edu>
2018-09-04 09:52:05 -0700

    router

--- programming/src/App.js
+++ programming/src/App.js
@@ -1,18 +1,39 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
+
+const pageOne = () => {
+  return (
+    <div>
+      Page1
+    </div>
+  );
+}
+
+const pageTwo = () => {
+  return (
+    <div>
+      Page2
+    </div>
+  );
+}
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <BrowserRouter>
+          <div>
+            <Switch>
+              <Route path="/one" component={pageOne} />
+              <Route exact path="/two" component={pageTwo} />
+              {/* <Route path="/blogs" component={Dashboard} /> */}
+              <Route path="/" component={pageOne} />
+            </Switch>
+          </div>
+        </BrowserRouter>
       </div>
     );
   }
