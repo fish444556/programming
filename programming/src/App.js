e8a800b2a0606f6b1264a48fd245ef870bcef950
fish444556 <jialinl@mtu.edu>
2018-09-05 07:39:44 -0700

    response header

--- programming/src/App.js
+++ programming/src/App.js
@@ -6,17 +6,37 @@ import PageOne from './components/PageOne';
 import PageTwo from './components/PageTwo';
 
 
-const Header = () => (
-  <div>
-    <nav>
-      <ul>
-        <li><Link to='/'>Home</Link></li>
-        <li><Link to='/one'>Add Number</Link></li>
-        <li><Link to='/two'>Change Color</Link></li>
-      </ul>
-    </nav>
-  </div>
-);
+class Header extends Component {
+  constructor(props) {
+    super(props);
+    this.handleToggle = this.handleToggle.bind(this);
+
+    this.state = {
+      panelDisplay: false
+    };
+  }
+
+  handleToggle() {
+    this.setState({
+      panelDisplay: !this.state.panelDisplay
+    })
+  }
+
+  render() {
+    const iconClassName = `topnav ${this.state.panelDisplay ? 'responsive' : ''}`;
+
+    return (
+      <nav className={iconClassName}>
+        <ul>
+          <li><Link to='/'>Home</Link></li>
+          <li><Link to='/one'>Add Number</Link></li>
+          <li><Link to='/two'>Change Color</Link></li>
+          <li className="icon" onClick={this.handleToggle}><i className="fa fa-bars"/></li>
+        </ul>
+      </nav>
+    );
+  }
+}
 
 class App extends Component {
   constructor(props) {
